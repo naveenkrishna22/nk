5f7e41a392c503aaa9e18adfbb78384afa381467
NAVEEN <naveenkrishna22998@gmail.com>
2023-05-25 09:52:54 +0530

    doneee

--- src/main/java/Vow/New.java
+++ src/main/java/Vow/New.java
@@ -9,17 +9,13 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Sheet;
 
 public class New {
-		String path="C:\\Users\\Admin\\eclipse-workspace\\Testingselenium\\foldsele\\samp.xls";
-		public void write(String Sheet1) throws IOException {
-			FileOutputStream f=new FileOutputStream(path);
-			HSSFWorkbook work = new HSSFWorkbook(f);
-			Sheet s=work.getSheet(Sheet1);
-			int row=s.getPhysicalNumberOfRows();
-			for(int i=row-1;i>=0;i--) {
+		
 				
-			}
+		public static void name() {
+			System.out.println("hello");
+		}	
 			
 			
 		}
 
-	}
+	
